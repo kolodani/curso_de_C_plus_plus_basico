94c7b21d554c3b3bc5865f10da68c6d16c1c0794
danielvinzia <danielvinzia@gmail.com>
2022-02-15 10:27:17 -0300

    clase de entrada y salida de datos concluida

---
+++ conceptos_basicos/05-entrada_y_salida_de_datos.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    // esto es un comentario
+    /*
+        esto es un comentario de multiples lineas
+    */
+    int edad = 0;
+    cout << "edad: ";
+    cin >> edad;
+    cout << "Tu edad es: " << edad;
+}
