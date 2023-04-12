1b7538c319f64b4805c1eb090a4cb4a1994fadac
kolodani <danielvinzia@gmail.com>
2023-04-12 19:19:35 -0300

    c++, 005 entrada y salida de datos

---
+++ 001-conceptos_basicos/005-entrada_y_salida_de_datos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // COMENTARIO DE UNA SOLA LINEA
+    /*
+        COMENTARIO
+        DE
+        VARIAS
+        LINEAS
+    */
+    int edad = 0;
+    cout << "Edad: ";
+    cin >> edad;
+    cout << "Tu edad es: " << edad << endl;
+}
